43a1e407494d9b6ea2010d350d8245c14269db5a
Charlotte Mannion <charlottemannion100@gmail.com>
2018-08-22 11:55:54 +0100

    Change 0 back to X - testing commits

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class Square extends React.Component {
 
       handleClick(i) {
           const squares = this.state.squares.slice();
-          squares[i] = '0';
+          squares[i] = 'X';
           this.setState({squares: squares});
       }
 
